41a7bcbbac072084a65bbc3e1666f215d59d67aa
Moses Enyinnaya <moscolyno98@gmail.com>
2024-09-20 16:44:55 +0200

    minor fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,29 +29,38 @@ const HomePage = () => {
   };
 
   return (
-    <div className="font-black" style={{ perspective: 1000 }}>
-      <motion.div
-        className="relative group w-full h-screen flex items-center justify-center overflow-hidden"
-        onMouseMove={handleMouseMove}
-      >
-        <motion.p
-          className="big-text opacity-0 hover:opacity-100 uppercase text-center max-w-lg 2xl:leading-[12vh] md:leading-[15vh] text-transparent mx-auto relative"
-          style={{
-            backgroundImage: useMotionTemplate`radial-gradient(200px circle at ${mouseTextX}px ${mouseTextY}px, red ,transparent 50%)`
-          }}
-          onMouseMove={handleMouseTextMove}
-        >
-          Will you marry me? ❤️
-        </motion.p>
+    <>
+      <div className="font-medium fixed top-8 left-8">
+        Move your mouse 🖱️
+        <br />
+        Use the light 🔦 <br />
+        Find the magic words 😉😌
+      </div>
 
+      <div className="font-black" style={{ perspective: 1000 }}>
         <motion.div
-          className="absolute -inset-px glow pointer-events-none opacity-0 group-hover:opacity-100 transition duration-300 shadow-inner blur-lg"
-          style={{
-            background: useMotionTemplate`radial-gradient(200px circle at ${mouseX}px ${mouseY}px,rgba(36, 36, 36, 0.8),transparent 50%)`
-          }}
-        ></motion.div>
-      </motion.div>
-    </div>
+          className="relative group w-full h-screen flex items-center justify-center overflow-hidden"
+          onMouseMove={handleMouseMove}
+        >
+          <motion.p
+            className="big-text opacity-0 hover:opacity-100 uppercase text-center max-w-lg 2xl:leading-[12vh] md:leading-[15vh] text-transparent mx-auto relative"
+            style={{
+              backgroundImage: useMotionTemplate`radial-gradient(200px circle at ${mouseTextX}px ${mouseTextY}px, red ,transparent 50%)`
+            }}
+            onMouseMove={handleMouseTextMove}
+          >
+            Will you marry me? ❤️
+          </motion.p>
+
+          <motion.div
+            className="absolute -inset-px glow pointer-events-none opacity-0 group-hover:opacity-100 transition duration-300 shadow-inner blur-lg"
+            style={{
+              background: useMotionTemplate`radial-gradient(200px circle at ${mouseX}px ${mouseY}px,rgba(36, 36, 36, 0.8),transparent 50%)`
+            }}
+          ></motion.div>
+        </motion.div>
+      </div>
+    </>
   );
 };
 

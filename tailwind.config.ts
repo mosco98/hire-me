562cabdb249e84521bbcd64cf2ac969379a30de2
Moses Enyinnaya <moscolyno98@gmail.com>
2024-09-20 12:40:31 +0200

    ready

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,16 +4,19 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
-        foreground: "var(--foreground)",
+        foreground: "var(--foreground)"
       },
-    },
+      fontFamily: {
+        sans: "var(--font-geist-sans)"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
